7d4431aac99c167d02a8d313d9b84fd03772a03e
Sergii Pylypenko <x.pelya.x@gmail.com>
2016-03-31 21:41:15 +0300

    Revert "Just messing around"
    This reverts commit 0387fb68ef0301b6c7907151c6d7b0627822fff1.

--- src/modules/module-opensles.c
+++ src/modules/module-opensles.c
@@ -43,332 +43,46 @@
 #include "module-opensles-symdef.h"
 
 PA_MODULE_AUTHOR("Sergii Pylypenko");
-PA_MODULE_DESCRIPTION("OpenSL ES Android Sink");
+PA_MODULE_DESCRIPTION("OpenSL ES Sink");
 PA_MODULE_VERSION(PACKAGE_VERSION);
-PA_MODULE_LOAD_ONCE(false);
 PA_MODULE_USAGE(
-        "sink_name=<name for the sink> "
-        "sink_properties=<properties for the sink> "
-        "format=<sample format> "
-        "rate=<sample rate> "
-        "channels=<number of channels> "
-        "channel_map=<channel map>");
+    "format=<sample format> "
+    "rate=<sample rate> "
+    "channels=<number of channels> "
+    "buffer=<buffer size> ");
+
+"opensles_output"
 
 struct userdata {
-    pa_core *core;
-    pa_module *module;
     pa_sink *sink;
+    pa_source *source;
+    pa_core *core;
+    pa_usec_t poll_timeout;
 
     pa_thread *thread;
     pa_thread_mq thread_mq;
     pa_rtpoll *rtpoll;
 
-    pa_memchunk memchunk;
-
-    pa_rtpoll_item *rtpoll_item;
-
-    int write_type;
-};
 
 static const char* const valid_modargs[] = {
-    "sink_name",
-    "sink_properties",
     "format",
     "rate",
     "channels",
-    "channel_map",
+    "buffer",
     NULL
 };
 
-static int sink_process_msg(pa_msgobject *o, int code, void *data, int64_t offset, pa_memchunk *chunk) {
-    struct userdata *u = PA_SINK(o)->userdata;
-
-    switch (code) {
-
-        case PA_SINK_MESSAGE_GET_LATENCY: {
-            size_t n = 0;
-
-/*
-            int l;
-
-            if (ioctl(u->fd, FIONREAD, &l) >= 0 && l > 0)
-                n = (size_t) l;
-*/
-            n += u->memchunk.length;
-
-            *((pa_usec_t*) data) = pa_bytes_to_usec(n, &u->sink->sample_spec);
-            return 0;
-        }
-    }
-
-    return pa_sink_process_msg(o, code, data, offset, chunk);
+static int process_msg(pa_msgobject *o, int code, void *data, int64_t offset, pa_memchunk *chunk) {
 }
 
-static int process_render(struct userdata *u) {
-    pa_assert(u);
-    /*
-    if (u->memchunk.length <= 0)
-        pa_sink_render(u->sink, pa_pipe_buf(u->fd), &u->memchunk);
-    */
-
-    pa_assert(u->memchunk.length > 0);
-
-    for (;;) {
-        ssize_t l;
-        void *p;
-
-        p = pa_memblock_acquire(u->memchunk.memblock);
-        //l = pa_write(u->fd, (uint8_t*) p + u->memchunk.index, u->memchunk.length, &u->write_type);
-        pa_memblock_release(u->memchunk.memblock);
-
-        pa_assert(l != 0);
-
-        if (l < 0) {
-
-            if (errno == EINTR)
-                continue;
-            else if (errno == EAGAIN)
-                return 0;
-            else {
-                pa_log("Failed to write data to FIFO: %s", pa_cstrerror(errno));
-                return -1;
-            }
-
-        } else {
-
-            u->memchunk.index += (size_t) l;
-            u->memchunk.length -= (size_t) l;
-
-            if (u->memchunk.length <= 0) {
-                pa_memblock_unref(u->memchunk.memblock);
-                pa_memchunk_reset(&u->memchunk);
-            }
-        }
-
-        return 0;
-    }
+static void sink_get_volume_cb(pa_sink *s) {
 }
 
-static void thread_func(void *userdata) {
-    struct userdata *u = userdata;
-
-    pa_assert(u);
-
-    pa_log_debug("Thread starting up");
-
-    pa_thread_mq_install(&u->thread_mq);
-
-    for (;;) {
-        struct pollfd *pollfd;
-        int ret;
-
-        pollfd = pa_rtpoll_item_get_pollfd(u->rtpoll_item, NULL);
-
-        if (PA_UNLIKELY(u->sink->thread_info.rewind_requested))
-            pa_sink_process_rewind(u->sink, 0);
-
-        /* Render some data and write it to the fifo */
-        if (PA_SINK_IS_OPENED(u->sink->thread_info.state)) {
-            if (pollfd->revents) {
-                if (process_render(u) < 0)
-                    goto fail;
-
-                pollfd->revents = 0;
-            }
-        }
-
-        /* Hmm, nothing to do. Let's sleep */
-        pollfd->events = (short) (u->sink->thread_info.state == PA_SINK_RUNNING ? POLLOUT : 0);
-
-        if ((ret = pa_rtpoll_run(u->rtpoll)) < 0)
-            goto fail;
-
-        if (ret == 0)
-            goto finish;
-
-        pollfd = pa_rtpoll_item_get_pollfd(u->rtpoll_item, NULL);
-
-        if (pollfd->revents & ~POLLOUT) {
-            pa_log("FIFO shutdown.");
-            goto fail;
-        }
-    }
-
-fail:
-    /* If this was no regular exit from the loop we have to continue
-     * processing messages until we received PA_MESSAGE_SHUTDOWN */
-    pa_asyncmsgq_post(u->thread_mq.outq, PA_MSGOBJECT(u->core), PA_CORE_MESSAGE_UNLOAD_MODULE, u->module, 0, NULL, NULL);
-    pa_asyncmsgq_wait_for(u->thread_mq.inq, PA_MESSAGE_SHUTDOWN);
-
-finish:
-    pa_log_debug("Thread shutting down");
-}
-
-int pa__init(pa_module *m) {
-    struct userdata *u;
-    struct stat st;
-    pa_sample_spec ss;
-    pa_channel_map map;
-    pa_modargs *ma;
-    struct pollfd *pollfd;
-    pa_sink_new_data data;
-
-    pa_assert(m);
-
-    if (!(ma = pa_modargs_new(m->argument, valid_modargs))) {
-        pa_log("Failed to parse module arguments.");
-        goto fail;
-    }
-
-    ss = m->core->default_sample_spec;
-    map = m->core->default_channel_map;
-    if (pa_modargs_get_sample_spec_and_channel_map(ma, &ss, &map, PA_CHANNEL_MAP_DEFAULT) < 0) {
-        pa_log("Invalid sample format specification or channel map");
-        goto fail;
-    }
-
-    u = pa_xnew0(struct userdata, 1);
-    u->core = m->core;
-    u->module = m;
-    m->userdata = u;
-    pa_memchunk_reset(&u->memchunk);
-    u->rtpoll = pa_rtpoll_new();
-    pa_thread_mq_init(&u->thread_mq, m->core->mainloop, u->rtpoll);
-    u->write_type = 0;
-
-    /*
-    u->filename = pa_runtime_path(pa_modargs_get_value(ma, "file", DEFAULT_FILE_NAME));
-
-    if (mkfifo(u->filename, 0666) < 0) {
-        pa_log("mkfifo('%s'): %s", u->filename, pa_cstrerror(errno));
-        goto fail;
-    }
-    if ((u->fd = pa_open_cloexec(u->filename, O_RDWR, 0)) < 0) {
-        pa_log("open('%s'): %s", u->filename, pa_cstrerror(errno));
-        goto fail;
-    }
-
-    pa_make_fd_nonblock(u->fd);
-
-    if (fstat(u->fd, &st) < 0) {
-        pa_log("fstat('%s'): %s", u->filename, pa_cstrerror(errno));
-        goto fail;
-    }
-
-    if (!S_ISFIFO(st.st_mode)) {
-        pa_log("'%s' is not a FIFO.", u->filename);
-        goto fail;
-    }
-    */
-
-    pa_sink_new_data_init(&data);
-    data.driver = __FILE__;
-    data.module = m;
-    pa_sink_new_data_set_name(&data, pa_modargs_get_value(ma, "sink_name", DEFAULT_SINK_NAME));
-    pa_proplist_sets(data.proplist, PA_PROP_DEVICE_STRING, u->filename);
-    pa_proplist_setf(data.proplist, PA_PROP_DEVICE_DESCRIPTION, "Unix FIFO sink %s", u->filename);
-    pa_sink_new_data_set_sample_spec(&data, &ss);
-    pa_sink_new_data_set_channel_map(&data, &map);
-
-    if (pa_modargs_get_proplist(ma, "sink_properties", data.proplist, PA_UPDATE_REPLACE) < 0) {
-        pa_log("Invalid properties");
-        pa_sink_new_data_done(&data);
-        goto fail;
-    }
-
-    u->sink = pa_sink_new(m->core, &data, PA_SINK_LATENCY);
-    pa_sink_new_data_done(&data);
-
-    if (!u->sink) {
-        pa_log("Failed to create sink.");
-        goto fail;
-    }
-
-    u->sink->parent.process_msg = sink_process_msg;
-    u->sink->userdata = u;
-
-    pa_sink_set_asyncmsgq(u->sink, u->thread_mq.inq);
-    pa_sink_set_rtpoll(u->sink, u->rtpoll);
-    /*
-    pa_sink_set_max_request(u->sink, pa_pipe_buf(u->fd));
-    pa_sink_set_fixed_latency(u->sink, pa_bytes_to_usec(pa_pipe_buf(u->fd), &u->sink->sample_spec));
-    */
-
-    /*
-    u->rtpoll_item = pa_rtpoll_item_new(u->rtpoll, PA_RTPOLL_NEVER, 1);
-    pollfd = pa_rtpoll_item_get_pollfd(u->rtpoll_item, NULL);
-    pollfd->fd = u->fd;
-    pollfd->events = pollfd->revents = 0;
-    */
-
-    if (!(u->thread = pa_thread_new("pipe-sink", thread_func, u))) {
-        pa_log("Failed to create thread.");
-        goto fail;
-    }
-
-    pa_sink_put(u->sink);
-
-    pa_modargs_free(ma);
-
-    return 0;
-
-fail:
-    if (ma)
-        pa_modargs_free(ma);
-
-    pa__done(m);
-
-    return -1;
+static void sink_set_volume_cb(pa_sink *s) {
 }
 
 int pa__get_n_used(pa_module *m) {
-    struct userdata *u;
-
-    pa_assert(m);
-    pa_assert_se(u = m->userdata);
-
-    return pa_sink_linked_by(u->sink);
 }
 
-void pa__done(pa_module *m) {
-    struct userdata *u;
-
-    pa_assert(m);
-
-    if (!(u = m->userdata))
-        return;
-
-    if (u->sink)
-        pa_sink_unlink(u->sink);
-
-    if (u->thread) {
-        pa_asyncmsgq_send(u->thread_mq.inq, NULL, PA_MESSAGE_SHUTDOWN, NULL, 0, NULL);
-        pa_thread_free(u->thread);
-    }
-
-    pa_thread_mq_done(&u->thread_mq);
-
-    if (u->sink)
-        pa_sink_unref(u->sink);
-
-    if (u->memchunk.memblock)
-        pa_memblock_unref(u->memchunk.memblock);
-
-    if (u->rtpoll_item)
-        pa_rtpoll_item_free(u->rtpoll_item);
-
-    if (u->rtpoll)
-        pa_rtpoll_free(u->rtpoll);
-
-    /*
-    if (u->filename) {
-        unlink(u->filename);
-        pa_xfree(u->filename);
-    }
-
-    if (u->fd >= 0)
-        pa_assert_se(pa_close(u->fd) == 0);
-    */
-
-    pa_xfree(u);
+int pa__init(pa_module *m) {
 }
